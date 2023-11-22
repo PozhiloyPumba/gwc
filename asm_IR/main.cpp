56dd9e1b376dc86def009100d53ea1f7958fd61c
PozhiloyPumba <ivanov.in@phystech.edu>
2023-11-22 22:36:52 +0300

    memset and allocaconst

--- asm_IR/main.cpp
+++ asm_IR/main.cpp
@@ -532,6 +532,60 @@ int main(int argc, char *argv[]) {
 
     builder.SetInsertPoint(main_entryBB);
 
+    // set const 
+    std::vector<std::vector<int>> arr = {
+        {425, 536, 13974511}, {151, 368, 3572079},  {440, 249, 1591737},
+        {3, 363, 4020334},    {715, 400, 6731484},  {58, 444, 15049432},
+        {512, 555, 13193426}, {311, 331, 2822364},  {37, 103, 2708939},
+        {656, 347, 505435},   {133, 358, 7584055},  {470, 494, 2201812},
+        {147, 36, 12949952},  {211, 396, 11288287}, {451, 494, 3202092},
+        {789, 392, 10526177}, {171, 367, 6475432},  {797, 484, 11629877},
+        {186, 228, 5563096},  {167, 351, 13589823}, {505, 250, 3208161},
+        {353, 284, 10462034}, {196, 117, 13461283}, {194, 257, 15139700},
+        {103, 276, 12291396}, {646, 144, 1097829},  {188, 217, 13558239},
+        {673, 10, 11114889},  {660, 520, 1556342},  {624, 169, 2951914},
+        {394, 43, 2414789},   {51, 14, 16103385},   {569, 373, 15724377},
+        {425, 523, 16143020}, {211, 568, 14574142}, {787, 224, 755813},
+        {712, 54, 1316433},   {651, 459, 1672796},  {525, 230, 15763870},
+        {129, 527, 10295048}, {631, 220, 9377424},  {4, 416, 12654176},
+        {601, 32, 6459850},   {252, 593, 5256075},  {54, 385, 14883775},
+        {115, 78, 8514895},   {604, 434, 3404590},  {20, 183, 1018311},
+        {7, 326, 12269577},   {73, 18, 6594307},    {0, 456, 10733656},
+        {479, 244, 2849870},  {16, 40, 7429552},    {322, 445, 7354436},
+        {429, 465, 12181642}, {633, 257, 5046100},  {39, 327, 8084592},
+        {256, 530, 16435783}, {367, 29, 9214164},   {245, 552, 1027154},
+        {232, 288, 14172151}, {93, 425, 10404819},  {530, 496, 2685554},
+        {205, 545, 9891471},  {110, 427, 7628983},  {298, 346, 11452609},
+        {633, 359, 3394207},  {609, 407, 3486714},  {398, 49, 15366155},
+        {63, 527, 6160162},   {311, 96, 105981},    {727, 201, 9827702},
+        {630, 316, 13555200}, {699, 169, 4481560},  {731, 347, 12098681},
+        {100, 381, 12245631}, {770, 247, 13267674}, {529, 123, 11677846},
+        {411, 211, 13468432}, {459, 342, 14129095}, {433, 181, 10508253},
+        {704, 268, 7340572},  {683, 273, 6896895},  {705, 3, 11830786},
+        {66, 490, 1521645},   {694, 326, 7597392},  {169, 359, 13177975},
+        {135, 265, 4607890},  {92, 451, 5972511},   {720, 29, 5711389},
+        {711, 582, 11496075}, {167, 469, 791786},   {44, 583, 2635077},
+        {630, 46, 9946804},   {262, 160, 6162150},  {457, 574, 14793056},
+        {415, 266, 13430405}, {16, 283, 7321896},   {5, 245, 3194004},
+        {331, 137, 3298150}};
+    std::vector<Constant *> init;
+    auto *arrayType3ints = ArrayType::get(Type::getInt32Ty(context), 3);
+
+    for (auto it = arr.begin(); it != arr.end(); ++it) {
+        std::vector<Constant *> tmp = {
+            ConstantInt::get(Type::getInt32Ty(context), (*it)[0], true),
+            ConstantInt::get(Type::getInt32Ty(context), (*it)[1], true),
+            ConstantInt::get(Type::getInt32Ty(context), (*it)[2], true)};
+        init.push_back(ConstantArray::get(arrayType3ints, tmp));
+    }
+
+    auto *arrayType100by3ints = ArrayType::get(arrayType3ints, 100);
+    GlobalVariable *garr_const =
+        new GlobalVariable(*module, static_cast<Type *>(arrayType100by3ints),
+                           true, GlobalValue::PrivateLinkage, 0, "arr");
+    garr_const->setAlignment(Align(16));
+    garr_const->setInitializer(ConstantArray::get(arrayType100by3ints, init));
+
     // Get pointer to CPU for function args
     Value *cpu_p = builder.getInt64((uint64_t)&cpu);
     Value *BB_p = builder.getInt64((uint64_t)&BB_PC);
@@ -564,13 +618,64 @@ int main(int argc, char *argv[]) {
         switch (instructions[PC].ID) {
         case FLUSH:
         case UPDATE_GPU_BUFFER:
-        case ALLOCA_COORDS_AND_COLORS:
-        case SET_ZEROS:
         case SET_PIXEL: {
             Value *instr_p = builder.getInt64((uint64_t) & (instructions[PC]));
             builder.CreateCall(module->getOrInsertFunction(
                                    "do_" + instructions[PC].name, CalleeType),
                                std::vector<Value *>({cpu_p, instr_p, BB_p}));
+            break;
+        }
+        case ALLOCA_COORDS_AND_COLORS: {
+            Value *res = builder.CreateConstGEP2_64(regFileType, regFile, 0,
+                                                    instructions[PC].rs1);
+            Value *imm = builder.getInt64(1200);
+            Type *ArrayTy =
+                ArrayType::get(builder.getInt8Ty(),
+                               static_cast<ConstantInt *>(imm)->getZExtValue());
+            Value *alloc = builder.CreateAlloca(ArrayTy);
+            builder.CreateStore(alloc, res);
+
+            auto *arg1 = builder.CreateConstGEP2_64(regFileType, regFile, 0,
+                                                     instructions[PC].rs1);
+            auto *argAddr = builder.CreateLoad(Type::getInt8PtrTy(context), arg1);
+            std::vector<Type *> memcpyParTypes = {
+                Type::getInt8PtrTy(context), Type::getInt8PtrTy(context),
+                Type::getInt64Ty(context), Type::getInt1Ty(context)};
+            std::vector<Value *> valuesFormemcpy1 = {
+                argAddr, builder.CreateBitCast(garr_const, Type::getInt8PtrTy(context)),
+                builder.getInt64(1200), builder.getInt1(false)};
+            auto setMemcpyAttr = [](auto *memcpy) {
+                memcpy->addParamAttr(0, Attribute::NoUndef);
+                memcpy->addParamAttr(0, Attribute::NonNull);
+
+                memcpy->addParamAttr(1, Attribute::NoUndef);
+                memcpy->addParamAttr(1, Attribute::NonNull);
+            };
+
+            auto *memcpy1 = builder.CreateIntrinsic(Intrinsic::memcpy, memcpyParTypes,
+                                                    valuesFormemcpy1);
+            setMemcpyAttr(memcpy1);
+            break;
+        }
+        case SET_ZEROS: {
+            std::vector<Type *> memsetParTypes = {
+                Type::getInt8PtrTy(context), Type::getInt8Ty(context),
+                Type::getInt64Ty(context), Type::getInt1Ty(context)};
+            auto setMemsetAttr = [](auto *memset) {
+                memset->addParamAttr(0, Attribute::NoUndef);
+                memset->addParamAttr(0, Attribute::NonNull);
+            };
+            auto *arg1 = builder.CreateConstGEP2_64(regFileType, regFile, 0,
+                                                     instructions[PC].rs1);
+            auto *argAddr = builder.CreateLoad(Type::getInt8PtrTy(context), arg1);
+
+            std::vector<Value *> valuesFormemset1 = {argAddr, builder.getInt8(0),
+                                                    builder.getInt64(int64_t(instructions[PC].imm)),
+                                                    builder.getInt1(false)};
+
+            auto *memset1 = builder.CreateIntrinsic(Intrinsic::memset, memsetParTypes,
+                                                    valuesFormemset1);
+            setMemsetAttr(memset1);
             break;
         }
         case RET: {
